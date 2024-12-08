b22f8a8417cfd97d091731aa8bee7bd6e8c5d2f1
nithin <158035569+nithinSai98@users.noreply.github.com>
2024-12-08 11:38:57 +0530

    serializer

---
+++ deploy/api/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+from .models import Employee
+
+class CustomUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Employee
+        fields = ['id', 'username', 'email', 'mobile_no']
+        extra_kwargs = {
+            'username': {'required': True},
+            'email': {'required': True},
+        }

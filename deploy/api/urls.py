d96704a2fe3bd24c1e73db723b49c46899594971
nithin <158035569+nithinSai98@users.noreply.github.com>
2024-12-10 09:05:52 +0530

    api files-- urls updated

--- deploy/api/urls.py
+++ deploy/api/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path
+from .views import UserCRUSView
 
 urlpatterns = [
-    # path('index/', ),
+    path('crus/', UserCRUSView.as_view(), name="crus"),
 ]
